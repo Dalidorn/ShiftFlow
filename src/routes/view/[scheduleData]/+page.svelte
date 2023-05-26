296a366cbb4ae504e5e69a71a02c66ae6f217179
Aliervo <aliervo@wingedvengeance.net>
2023-05-26 16:36:18 -0500

    Add basic schedule graph

--- src/routes/view/[scheduleData]/+page.svelte
+++ src/routes/view/[scheduleData]/+page.svelte
@@ -1,13 +1,96 @@
 <script>
-  import lz from "lz-string";
+	import lz from 'lz-string';
+	import { scaleTime, scaleBand, scaleOrdinal } from 'd3-scale';
+	import { schemeCategory10 } from 'd3-scale-chromatic';
 
-  export let data;
-  
-  let schedule = JSON.parse(lz.decompressFromEncodedURIComponent(data.scheduleData));
+	export let data;
+
+	const schedule = categorize(JSON.parse(lz.decompressFromEncodedURIComponent(data.scheduleData)));
+	const timeRange = schedule.flatMap((shift) => [
+		new Date(shift.start).valueOf(),
+		new Date(shift.end).valueOf()
+	]);
+	const timeDim = scaleTime([Math.min(...timeRange), Math.max(...timeRange)], [0, 100]).nice();
+	const barDim = scaleBand(
+		schedule.map((d) => d.category),
+		[13, 30]
+	).paddingInner(0.1);
+	const nameDim = scaleOrdinal(
+		schedule.map((d) => d.name),
+		schemeCategory10
+	);
+
+	function categorize([first, ...rest], accumulator = []) {
+		// Find any that don't overlap. This is when:
+		// |------A------| |------B------| A before B
+		// |------B------| |------A------| B before A
+		const noOverlap = rest.filter((ele) => ele.end < first.start || ele.start > first.end);
+
+		// Find any in the noOverlap array that dont overlap with each other
+		function removeOverlaps([start, ...end], acc, category) {
+			// An element overlaps when:
+			// |--------A----|~~~~~|-------B---------------| A before B
+			// |------|~~~~~~A~~~~~~|----------B-----------| A during B
+			// |--------B----|~~~~~|-------A---------------| A after B
+			// |------|~~~~~~B~~~~~~|----------A-----------| B during A
+			const overlap = acc.some((ele) => {
+				return (
+					(ele.start < start.start && ele.end > start.start) || // A before B
+					(ele.start > start.start && ele.end < start.end) || // A during B
+					(ele.start > start.start && ele.start < start.end) || // B before A
+					(ele.start < start.start && ele.end > start.end)
+				); // B during A
+			});
+
+			return !start
+				? acc
+				: end.length === 0
+				? overlap
+					? acc
+					: [...acc, { ...start, category: category }]
+				: overlap
+				? removeOverlaps(end, acc, category)
+				: removeOverlaps(end, [...acc, { ...start, category: category }], category);
+		}
+
+		const lessOverlaps = removeOverlaps(noOverlap, [], first._id);
+		const uncategorized = rest.filter((e) => !lessOverlaps.map((o) => o._id).includes(e._id));
+
+		return rest.length === 0 || uncategorized.length === 0
+			? [...accumulator, { ...first, category: first._id }, ...lessOverlaps]
+			: categorize(uncategorized, [
+					...accumulator,
+					{ ...first, category: first._id },
+					...lessOverlaps
+			  ]);
+	}
 </script>
 
-<p>Here's your data!</p>
+<svg viewBox="-5 -5 110 40">
+	<line x1="0" y1="12" x2="100" y2="12" stroke="black" />
+	{#each timeDim.ticks() as tick}
+		<path id={timeDim(tick)} d="M {timeDim(tick)},12 {timeDim(tick) + 2},0" stroke="black" />
+		<text>
+			<textPath href="#{timeDim(tick)}" startOffset="5%">{tick}</textPath>
+		</text>
+	{/each}
+	{#each schedule as { start, end, category, name }}
+		<rect
+			x={timeDim(new Date(start))}
+			y={barDim(category)}
+			width={timeDim(new Date(end)) - timeDim(new Date(start))}
+			height={barDim.bandwidth()}
+			fill={nameDim(name)}
+		/>
+	{/each}
+</svg>
 
-{#each schedule as shift}
-  <p>{shift.name}</p>
-{/each}
+<style>
+	svg {
+		width: 95%;
+		stroke-width: .1px;
+	}
+	text {
+		font-size: 12%;
+	}
+</style>
